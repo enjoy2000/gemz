d00a5b292c62c23f5aab0b18e1942c7980970c1c
Gemz <gemzgallery@gmail.com>
2015-09-16 06:32:28 -0700

    Fix some on module config

--- app/code/local/Nik/Newsletterpopup/controllers/SubscriberController.php
+++ app/code/local/Nik/Newsletterpopup/controllers/SubscriberController.php
@@ -1,5 +1,5 @@
 <?php
-require_once Mage::getModuleDir('controllers', 'Mage_Newsletter') . DS . 'SubscriberController.php';
+require_once 'app/code/core/Mage/Newsletter/controllers/SubscriberController.php';
 
 class Nik_Newsletterpopup_SubscriberController extends Mage_Newsletter_SubscriberController
 {
@@ -20,7 +20,7 @@ class Nik_Newsletterpopup_SubscriberController extends Mage_Newsletter_Subscribe
 
         // get poll result to send email
         if ($pollResult = $this->getRequest()->getPost('poll')) {
-            if (isset($status) && $status != Mage_Newsletter_Model_Subscriber::STATUS_NOT_ACTIVE) {
+            //if (isset($status) && $status != Mage_Newsletter_Model_Subscriber::STATUS_NOT_ACTIVE) {
                 $email = (string)$this->getRequest()->getPost('email');
                 $poll = [
                     '1' => 'I only buy it to get out of trouble',
@@ -50,7 +50,7 @@ class Nik_Newsletterpopup_SubscriberController extends Mage_Newsletter_Subscribe
                     Mage::log($e->getMessage());
                     Mage::getSingleton('core/session')->addError($e->getMessage());
                 }
-            }
+            //}
         }
     }
 }
