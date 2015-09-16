61aef15d821e74457917c6b798cb6f2b4346b2fb
Hat Dao <enjoy3013@gmail.com>
2015-09-16 18:06:11 +0700

    tmp for newsletter

--- app/code/local/Nik/Newsletterpopup/controllers/SubscriberController.php
+++ app/code/local/Nik/Newsletterpopup/controllers/SubscriberController.php
@@ -17,5 +17,17 @@ class Nik_Newsletterpopup_SubscriberController extends Mage_Newsletter_Subscribe
             if (isset($_COOKIE['email_subscribed']))
                 setcookie('email_subscribed', $email, time() - $timeCookiesTimeout * 86400, '/');
         }
+
+        // get poll result to send email
+        if ($pollResult = $this->getRequest()->getPost('poll')) {
+            if (isset($status) && $status == Mage_Newsletter_Model_Subscriber::STATUS_NOT_ACTIVE) {
+                $poll = [
+                    '1' => 'I only buy it to get out of trouble',
+                    '2' => 'It looks good on my Pinterest page',
+                    '3' => 'I buy seasonal pieces at walmart',
+                    '4' => 'I love it! Please keep me informed...',
+                ];
+            }
+        }
     }
 }
