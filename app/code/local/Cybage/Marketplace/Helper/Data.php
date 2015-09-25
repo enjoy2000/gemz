aafedfb070c46a54fa2b3419351f1d99c20288de
Hat Dao <enjoy3013@gmail.com>
2015-09-25 13:13:51 +0700

    Resize seller profile image

--- app/code/local/Cybage/Marketplace/Helper/Data.php
+++ app/code/local/Cybage/Marketplace/Helper/Data.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Cybage Marketplace Plugin
  *
@@ -17,7 +18,6 @@
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  * @author     Cybage Software Pvt. Ltd. <Support_Magento@cybage.com>
  */
-
 class Cybage_Marketplace_Helper_Data extends Mage_Core_Helper_Abstract
 {
     const MARKETPLACE_PRODUCT_VISIBILITY = 'marketplace/product/visibility';
@@ -36,26 +36,30 @@ class Cybage_Marketplace_Helper_Data extends Mage_Core_Helper_Abstract
     /**
      * Collection of Allowed Category for Marketplace
      * */
-    public function getCategoryCollection() {
+    public function getCategoryCollection()
+    {
         return Mage::getModel('catalog/category')->getCollection()
-                        ->addAttributeToFilter('is_active', 1)
-                        ->addAttributeToFilter('category_marketplace', 1) //category_marketplace
-        ;
+            ->addAttributeToFilter('is_active', 1)
+            ->addAttributeToFilter('category_marketplace', 1) //category_marketplace
+            ;
     }
 
-    public function getNewProductVisibility() {
+    public function getNewProductVisibility()
+    {
 
         return Mage::getStoreConfig(self::MARKETPLACE_PRODUCT_VISIBILITY);
     }
 
-    public function isMarketplaceEnabled() {
-        return (bool) Mage::getStoreConfig(self::MARKETPLACE_ENABLE, Mage::app()->getStore());
+    public function isMarketplaceEnabled()
+    {
+        return (bool)Mage::getStoreConfig(self::MARKETPLACE_ENABLE, Mage::app()->getStore());
     }
 
-    public function isMarketplaceActiveSellar() {
+    public function isMarketplaceActiveSellar()
+    {
         if ($this->isMarketplaceEnabled()) {
             $customer = Mage::getSingleton('customer/session')->getCustomer();
-            if ($customer->getStatus() == Mage::getStoreConfig(self::MARKETPLACE_STATUS_APPROVED) && (bool) $customer->getSellerSubscriber()) {
+            if ($customer->getStatus() == Mage::getStoreConfig(self::MARKETPLACE_STATUS_APPROVED) && (bool)$customer->getSellerSubscriber()) {
                 return true;
             } else {
                 return false;
@@ -65,53 +69,61 @@ class Cybage_Marketplace_Helper_Data extends Mage_Core_Helper_Abstract
         }
     }
 
-    public function getNewProductStatus() {
+    public function getNewProductStatus()
+    {
 
         return Mage::getStoreConfig(self::MARKETPLACE_PRODUCT_STATUS);
     }
 
-    public function getNewProductState() {
+    public function getNewProductState()
+    {
 
         return Mage::getStoreConfig(self::MARKETPLACE_PRODUCT_STATE);
     }
 
-    public function getNewProductUploadImageSize($check=null) {
+    public function getNewProductUploadImageSize($check = null)
+    {
         if ($check == 'validate') {
             return Mage::getStoreConfig(self::MARKETPLACE_PRODUCT_UPLOAD_IMAGE_SIZE);
         } else {
-            return round(Mage::getStoreConfig(self::MARKETPLACE_PRODUCT_UPLOAD_IMAGE_SIZE)/(1024 * 1024), 2) .' MB';
+            return round(Mage::getStoreConfig(self::MARKETPLACE_PRODUCT_UPLOAD_IMAGE_SIZE) / (1024 * 1024), 2) . ' MB';
         }
     }
 
     /**
      *    Method : To get Product Pending Status Id
      * */
-    public function getProductPendingStatusId() {
+    public function getProductPendingStatusId()
+    {
         return Mage::getStoreConfig(self::MARKETPLACE_PRODUCT_PENDING);
     }
 
     /**
      *    Method : To get Product Pending Status Id
      * */
-    public function getProductApprovedStatusId() {
+    public function getProductApprovedStatusId()
+    {
         return Mage::getStoreConfig(self::MARKETPLACE_PRODUCT_APPROVED);
     }
 
     /**
      *    Method : To get Product Pending Status Id
      * */
-    public function getProductRejectedStatusId() {
+    public function getProductRejectedStatusId()
+    {
         return Mage::getStoreConfig(self::MARKETPLACE_PRODUCT_REJECTED);
     }
 
     /**
      *    Method : To get Product Pending Status Id
      * */
-    public function getProductDeletedStatusId() {
+    public function getProductDeletedStatusId()
+    {
         return Mage::getStoreConfig(self::MARKETPLACE_PRODUCT_DELETED);
     }
 
-    public function getDeletedOptionValue() {
+    public function getDeletedOptionValue()
+    {
         $attribute = Mage::getModel('eav/config')->getAttribute('catalog_product', 'marketplace_state');
         $optionValue = "";
         foreach ($attribute->getSource()->getAllOptions(true, true) as $option) {
@@ -126,7 +138,8 @@ class Cybage_Marketplace_Helper_Data extends Mage_Core_Helper_Abstract
         }
     }
 
-    public function getApprovedOptionValue() {
+    public function getApprovedOptionValue()
+    {
         $attribute = Mage::getModel('eav/config')->getAttribute('catalog_product', 'marketplace_state');
         $optionValue = "";
         foreach ($attribute->getSource()->getAllOptions(true, true) as $option) {
@@ -141,15 +154,18 @@ class Cybage_Marketplace_Helper_Data extends Mage_Core_Helper_Abstract
         }
     }
 
-    public function getSku($productId) {
+    public function getSku($productId)
+    {
         return Mage::getModel('catalog/product')->load($productId)->getSku();
     }
 
-    public function getProductName($productId) {
+    public function getProductName($productId)
+    {
         return Mage::getModel('catalog/product')->load($productId)->getName();
     }
 
-    public function isMarketplaceApprovedProduct($currentProduct) {
+    public function isMarketplaceApprovedProduct($currentProduct)
+    {
         $marketplaceState = $currentProduct->getMarketplaceState();
         $marketplaceApprovedOptionValue = $this->getApprovedOptionValue();
 
@@ -160,7 +176,8 @@ class Cybage_Marketplace_Helper_Data extends Mage_Core_Helper_Abstract
         }
     }
 
-    public function isSelfProduct($currentProduct) {
+    public function isSelfProduct($currentProduct)
+    {
 
         $sellerId = $currentProduct->getSellerId();
         $customerId = Mage::getSingleton('customer/session')->getCustomer()->getId();
@@ -176,18 +193,20 @@ class Cybage_Marketplace_Helper_Data extends Mage_Core_Helper_Abstract
      * @return customer object
      * @author srinidhid@cybage.com
      */
-    public function getSellerInfo($productSellerName) {
+    public function getSellerInfo($productSellerName)
+    {
         $customer = Mage::getModel('customer/customer')
-                ->getCollection()
-                ->addFieldToFilter('company_name', $productSellerName)
-                ->getFirstItem()
-                ->load();
+            ->getCollection()
+            ->addFieldToFilter('company_name', $productSellerName)
+            ->getFirstItem()
+            ->load();
         return $customer->getData();
     }
 
     /* To fetch MarketOrders From sales ordered to seller in Csv */
 
-    public function getMarketOrders() {
+    public function getMarketOrders()
+    {
         $dateTo = Mage::getSingleton('core/session')->getDateTo();
         $dateFrom = Mage::getSingleton('core/session')->getDateFrom();
         $orderStatus = Mage::getSingleton('core/session')->getSalesOrderSatus();
@@ -195,14 +214,14 @@ class Cybage_Marketplace_Helper_Data extends Mage_Core_Helper_Abstract
         $sellerId = Mage::getSingleton('customer/session')->getId();
 
         $orderItemCollection = Mage::getResourceModel('sales/order_item_collection')
-                ->addFieldToSelect('order_id')
-                ->addFieldToFilter('main_table.seller_id', $sellerId)
-                ->distinct(true);
+            ->addFieldToSelect('order_id')
+            ->addFieldToFilter('main_table.seller_id', $sellerId)
+            ->distinct(true);
         $resource = Mage::getSingleton("core/resource");
-        $tblSalesFlatOrder = $resource->getTableName('sales_flat_order');        
+        $tblSalesFlatOrder = $resource->getTableName('sales_flat_order');
         $orderItemCollection->getSelect()
-                ->join($tblSalesFlatOrder, $tblSalesFlatOrder.'.entity_id=main_table.order_id', array('increment_id', 'status'))
-                ->columns(new Zend_Db_Expr("CONCAT(`$tblSalesFlatOrder`.`customer_firstname`, ' ',`$tblSalesFlatOrder`.`customer_lastname`) AS billname"));
+            ->join($tblSalesFlatOrder, $tblSalesFlatOrder . '.entity_id=main_table.order_id', array('increment_id', 'status'))
+            ->columns(new Zend_Db_Expr("CONCAT(`$tblSalesFlatOrder`.`customer_firstname`, ' ',`$tblSalesFlatOrder`.`customer_lastname`) AS billname"));
 
         $orderItemCollection->getSelect()->columns('SUM(row_total + shipping_charges) AS Total');
         $orderItemCollection->getSelect()->columns('SUM(row_invoiced) AS Amount Received');
@@ -213,23 +232,24 @@ class Cybage_Marketplace_Helper_Data extends Mage_Core_Helper_Abstract
         $dateTo = date('Y-m-d H:i:s', strtotime($dateTo . "+1 day"));
 
         if ($dateFrom != '') {
-            $orderItemCollection->addFieldToFilter($tblSalesFlatOrder.'.created_at', array('from' => "$dateFrom"));
+            $orderItemCollection->addFieldToFilter($tblSalesFlatOrder . '.created_at', array('from' => "$dateFrom"));
         }
         if ($dateTo != '') {
-            $orderItemCollection->addFieldToFilter($tblSalesFlatOrder.'.created_at', array('to' => "$dateTo"));
+            $orderItemCollection->addFieldToFilter($tblSalesFlatOrder . '.created_at', array('to' => "$dateTo"));
         }
         if ($orderStatus != '') {
-            $orderItemCollection->addFieldToFilter($tblSalesFlatOrder.'.status', "$orderStatus");
+            $orderItemCollection->addFieldToFilter($tblSalesFlatOrder . '.status', "$orderStatus");
         }
 
-        return($orderItemCollection);
+        return ($orderItemCollection);
     }
 
     /**
      * Generates CSV file with product's list according to the collection in the $this->_list
      * @return array
      */
-    public function generateMarketCsv() {
+    public function generateMarketCsv()
+    {
         $this->getMarketListOrders();
         if (!is_null($this->_list)) {
             $orders = $this->_list->getItems();
@@ -256,7 +276,7 @@ class Cybage_Marketplace_Helper_Data extends Mage_Core_Helper_Abstract
                 }
 
                 return array
-                    (
+                (
                     'type' => 'filename',
                     'value' => $file,
                     'rm' => true
@@ -267,7 +287,8 @@ class Cybage_Marketplace_Helper_Data extends Mage_Core_Helper_Abstract
 
     /* Retrieve Market list to export data orders */
 
-    public function getMarketListOrders() {
+    public function getMarketListOrders()
+    {
         $orders = $this->getMarketOrders();
         $this->setList($orders);
     }
@@ -276,30 +297,33 @@ class Cybage_Marketplace_Helper_Data extends Mage_Core_Helper_Abstract
      * Sets current collection
      * @param $query
      */
-    public function setList($collection) {
+    public function setList($collection)
+    {
         $this->_list = $collection;
     }
 
     /* Generate headers for CSV file */
 
-    protected function _getCsvHeaders() {
+    protected function _getCsvHeaders()
+    {
         $headers = array('Order#', 'Bill to Name', 'Status', 'Total sales', 'Amount Received', 'Amount Remain');
         return $headers;
     }
 
     /**
-      Get order sutotal.shipping charge and grand total for specific seller.
+     * Get order sutotal.shipping charge and grand total for specific seller.
      * */
-    public function getOrderTotals($orderId) {
+    public function getOrderTotals($orderId)
+    {
         $marketPlaceModel = Mage::getModel('marketplace/order');
         $orderItemCollection = $marketPlaceModel->getOrderDetails($orderId);
 
         $orderTotal = array();
-        $orderTotal['subtotal'] = 0;  
+        $orderTotal['subtotal'] = 0;
         $orderTotal['shippingcharge'] = 0;
         foreach ($orderItemCollection as $_item) {
-            $orderTotal['subtotal'] += (float) $_item->getRowTotal();
-            $orderTotal['shippingcharge'] += (float) $_item->getShippingCharges();
+            $orderTotal['subtotal'] += (float)$_item->getRowTotal();
+            $orderTotal['shippingcharge'] += (float)$_item->getShippingCharges();
         }
 
         $orderTotal['grandtotal'] = $orderTotal['subtotal'] + $orderTotal['shippingcharge'];
@@ -311,21 +335,23 @@ class Cybage_Marketplace_Helper_Data extends Mage_Core_Helper_Abstract
      * @param seller_id
      * @return customer name
      */
-    public function getSellerName($seller_id) {
+    public function getSellerName($seller_id)
+    {
         $customer = Mage::getModel('customer/customer')->load($seller_id);
         return $customer->getCompanyName();
     }
 
-    public function getProductSellerAttributeId(){
-       $_sellerAttrId = $_attributeData = '';
-       $_attributeData = Mage::getModel('eav/entity_attribute')->getCollection()
-       ->setEntityTypeFilter(Mage::getModel('catalog/product')->getResource()->getTypeId())
-       ->addFieldToFilter('attribute_code', 'seller_id'); 
+    public function getProductSellerAttributeId()
+    {
+        $_sellerAttrId = $_attributeData = '';
+        $_attributeData = Mage::getModel('eav/entity_attribute')->getCollection()
+            ->setEntityTypeFilter(Mage::getModel('catalog/product')->getResource()->getTypeId())
+            ->addFieldToFilter('attribute_code', 'seller_id');
 
-        if($_attributeData){
-           foreach($_attributeData as $_data){
-               $_sellerAttrId = $_data['attribute_id'];
-           }
+        if ($_attributeData) {
+            foreach ($_attributeData as $_data) {
+                $_sellerAttrId = $_data['attribute_id'];
+            }
         }
 
         return $_sellerAttrId;
